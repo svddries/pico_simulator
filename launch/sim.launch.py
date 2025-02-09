c7e59d81b7db149f2f2d20c200428cc1f404e549
Wiktor <w.bocian@student.tue.nl>
2025-02-09 21:02:06 +0100

    Automatic launch of map_server with laaunch file

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
 from launch_ros.actions import Node
+import launch_ros.actions
 
 def generate_launch_description():
     # Get the package directory dynamically
@@ -60,11 +61,33 @@ def generate_launch_description():
         output="screen"
     )
 
+    lifecycle_nodes = ['map_server']
+    use_sim_time = True
+    autostart = True
+
+    start_lifecycle_manager_cmd = launch_ros.actions.Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager',
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes}])
+
+
+
+
+    LaunchDescription().add_action(map_server)
+    LaunchDescription().add_action(start_lifecycle_manager_cmd)
+
+
     return LaunchDescription([
         config_arg,
         map_arg,
         # robot_state_publisher,
         # joint_state_publisher,
         map_server,
-        simulator
+        simulator,
+        start_lifecycle_manager_cmd
     ])
